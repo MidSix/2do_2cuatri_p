1fcb02def586267fd29d76ae8f471019503204ba
Acrafto <xoel.sanchez.dacoba@udc.es>
2026-02-09 17:49:08 +0100

    Feat(ALF:P1): Completado ejercicio 2 parte opcional

--- ALF/p/p1/mg.py
+++ ALF/p/p1/mg.py
@@ -17,6 +17,10 @@ def mg_int(n:int)->bool:
     return True
 
 def mg_str(chain:str)->bool: #Apartado opcional, este era chill
+    chain=chain.replace(" ","")#Entendemos que los espacios no cuentan, así que los eliminamos para facilitar cálculos posteriores
+    if chain=="mg":#Si la cadena es "mg", es correcta(Esto es para ahorrar cálculos posteriores, aunque no deberia ser necesario)
+        return print("Yes")
+    
     indx_m=chain.index("m")
     indx_g=chain.index("g")
     
@@ -26,32 +30,29 @@ def mg_str(chain:str)->bool: #Apartado opcional, este era chill
     ):
         return print("No")
     #Apartir de aquí, sabemos que la cadena es del tipo ∼^x m ∼^y g ∼^z, con x,y,z>=0, y que solo hay un m y un g
-    if len(chain)==2:#Si la cadena es "mg", es correcta(Esto es para ahorrar cálculos posteriores, aunque no deberia ser necesario)
-        return print("Yes")
     
     x=indx_m#Todo lo que hay antes de m son ∼
     y=indx_g-(indx_m+1)#Todo lo que hay entre m y g son ∼
     z=len(chain)-(indx_g+1)#Todo lo que hay después de g son ∼
     
     if ((z==0 or y==0)#Es imposible obtener cadenas de con y=0 o z=0 
-    or (x>=z)#Si x es mayor o igual a z, es incorrecta
+    or ((x>=z) or (y>z))#Si x es mayor o igual a z, es incorrecta, o si y es mayor que z, también lo es
     ): 
         return print("No")
+    
     
     return print("Yes")#Si se cumplen las condiciones anteriores, la cadena es correcta
 
     
 
-
 if __name__ == "__main__":
-    mg_str("mg")
-if __name__ == "__notmain__":
-    #$ python mg.py int or $ python mg.py string
+    #$ python mg.py -n int or $ python mg.py -s string
     parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument('n', type=int, help='An integer for the accumulator')
-    parser.add_argument('s', type=str, help='A string for the accumulator, example: "mg" or "∼m∼g"')
+    parser.add_argument('-n', type=int, help='An integer for the accumulator')
+    parser.add_argument('-s', type=str, help='A string to check, example: "mg" or "∼m∼g"')
     args = parser.parse_args()
     if args.n is not None:
         mg_int(args.n)
     elif args.s is not None:
         mg_str(args.s)
+
