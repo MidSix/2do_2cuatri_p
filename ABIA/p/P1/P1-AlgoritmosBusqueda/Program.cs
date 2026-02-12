0edd14d44aca9722581a1467e985da17efb571d3
Acrafto <xoel.sanchez.dacoba@udc.es>
2026-02-12 19:25:55 +0100

    Feat(ABIA): Añadidos los documentos de la P1 y empezado la tarea 1

---
+++ ABIA/p/P1/P1-AlgoritmosBusqueda/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Miembros:
+    - Xoel Sánchez Dacoba
+    - Sebastián David Moreno Expósito
+Grupo de prácticas:
+    G1.1 - jueves.
+*/
+Console.WriteLine("Hello, World!");
+
---
+++ ABIA/p/P1/P1-AlgoritmosBusqueda/Semana_1.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp1;
+
+public class Solucion
+{
+    public int Coste { get; set; }
+    public Tuple<int,int> Coords { get; set; }
+
+    public static bool Eq( Tuple<int,int> Coords,int Coste)
+    {
+        return str()==1;
+    }
+
+
+}
