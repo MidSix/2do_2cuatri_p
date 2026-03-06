11b557e88e1e0a74f646b420ab3b452a472765c0
Acrafto <xoel.sanchez.dacoba@udc.es>
2026-03-06 18:57:26 +0100

    Feat(ALF):Avanzado minimamente en el planteamiento del automatron.

--- ABIA/p/P1/P1-AlgoritmosBusqueda/obj/Debug/net10.0/P1-AlgoritmosBusqueda.AssemblyInfo.cs
+++ ABIA/p/P1/P1-AlgoritmosBusqueda/obj/Debug/net10.0/P1-AlgoritmosBusqueda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P1-AlgoritmosBusqueda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4658e475d3fc1955687f5314fd6ce7ee18c1def8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf94f8a9cffa1f122dac604e3df1e5e1965311a")]
 [assembly: System.Reflection.AssemblyProductAttribute("P1-AlgoritmosBusqueda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P1-AlgoritmosBusqueda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ALF/p/p2/siw.py
+++ ALF/p/p2/siw.py
@@ -1,18 +1,52 @@
- 
-def read_bin(filename)->list:
+class Automaton:
     """
-    Leemos el archivo binario y lo almacenamos en una lista de enteros.
+    Tratamos de crear una especie de lista posicionada que nos permita aplicar las transiciones,
+    para ello haremos uso de esta clase con la subclase estado.
+    Lo que sabemos por el enunciado es que la primera celda contiene el número de celdas total
     """
-    automatron_bin = []
-    with open(filename, 'rb') as f:
-        while True:
-            byte = f.read(1)
-            if not byte:
-                break
-            automatron_bin.append(byte[0])
-    return automatron_bin
+    def __init__(self, binary_file):
+        self.bin = self.read_bin(binary_file)
+        self.cells=int.from_bytes(self.bin[0:4], byteorder='little')#Total de celdad
+        self.automaton = self.gen_table(self.bin)
 
-def format_table(table):
+    def read_bin(filename)->list:
+        """
+        Leemos el archivo binario y lo almacenamos en una lista de enteros.
+        """
+        automatron_bin = []
+        with open(filename, 'rb') as f:
+            while True:
+                byte = f.read(1)
+                if not byte:
+                    break
+                automatron_bin.append(byte[0])
+        return automatron_bin
+    
+    def gen_table(self):
+        return None
+
+    class state:
+        """
+        Un estado contendrá:
+        • Primera celda, con la información del estado:
+        ◦ 1 byte: Número de celdas consecutivas (transiciones) que definen ese estado.
+        ◦ 3 bytes: Peso de la indexación.
+        • Celdas con información de las transiciones:
+        ◦ 3 byte: Carácter que representa la transición.
+        ◦ 1 byte: Celda destino de la transición
+        """
+        def __init__(self, transitions):
+            self.weight = 0 #Inicialmente valen 0, desde la clase matriz ya emplearemos un algotimo de asignacion
+            self.transitions = transitions
+            self.cells
+
+
+
+
+
+
+            
+def format_table(table)->tuple:
     """Formateamos la tabla del automata sabemos que su estructura es la siguiente:
     los primeros 4 bytes son el número de celdas(autoexcluyendose), los siguientes 4 bytes 
     son el estado final de ahi en adelante tenemos:
@@ -20,11 +54,31 @@ def format_table(table):
     ◦ 1 byte: Número de celdas consecutivas (transiciones) que definen ese estado.
     ◦ 3 bytes: Peso de la indexación.
     • Celdas con información de las transiciones:
-    ◦ 1 byte: Carácter que representa la transición.
-    ◦ 3 bytes: Celda destino de la transición
+    ◦ 3 byte: Carácter que representa la transición.
+    ◦ 1 byte: Celda destino de la transición
     Sabiendo esto almacenaremos una matriz con cada estado, sus transiciones y sus pesos(inicialmente 0).
-    de la siguiente manera
+    de la siguiente manera: el automatron será una matriz de listas, cada sublista será un estado y cada celda dentro de ese
+    sublista será una tupla con el peso y las transiciones, cada transición será una tupla con el caracter y la celda destino.
+    este automatron se retornará. 
     """
+    bytes_per_cell = 4
+    num_cells = int.from_bytes(table[0:4], byteorder='little')
+    final_state = int.from_bytes(table[4:8], byteorder='little')
+    automaton = []
+    index = 8
+    for _ in range(num_cells):
+        num_transitions = table[index]
+        weight = int.from_bytes(table[index+1:index+4], byteorder='little')
+        index += bytes_per_cell
+        transitions = []
+        for _ in range(num_transitions):
+            char = table[index]
+            dest_cell = int.from_bytes(table[index+1:index+4], byteorder='little')
+            transitions.append((char, dest_cell))
+            index += bytes_per_cell
+        automaton.append((weight, transitions))
+    return automaton, final_state
+
 def show_table(table):
     for i in range(0, len(table), 16):
         print(' '.join(f'{byte:02x}' for byte in table[i:i+16]))
@@ -32,5 +86,6 @@ def show_table(table):
 if __name__ == "__main__":
     relative_path = './Automatas_Diccionarios/'
     filename = f'{relative_path}tiny.bin'
-    automatron_table = read_bin(filename)
+    automatron_table= read_bin(filename)
+    automaton, final_state = format_table(automatron_table)
     show_table(automatron_table)
